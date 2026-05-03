cef35c8e6aed2bd5e30f56de057f09f070e9617c
kasekidev <teachbs20ktr@gmail.com>
2026-05-03 11:48:34 +0100

    cet empaffé de yacine a t-il supp un truc?

--- Backend/app/schemas/__init__.py
+++
@@ -1,3 +0,0 @@
-from app.schemas.order import OrderCreate, OrderResponse, OrderStatusUpdate
-from app.schemas.schemas_product import CategoryCreate, CategoryResponse, ProductCreate, ProductResponse
-from app.schemas.user import UserLogin, UserRegister, UserResponse, UserUpdate
